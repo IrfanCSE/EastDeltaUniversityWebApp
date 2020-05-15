13de23a922fbba0b4474e01a92668521b5971cf2
IrfanulHasan <irfanulhasan1996@gmail.com>
2020-05-15 17:45:11 +0600

    First

---
+++ EastDeltaUniversity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EastDeltaUniversity.MvcApplication" Language="C#" %>
---
+++ EastDeltaUniversity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EastDeltaUniversity.Startup))]
+namespace EastDeltaUniversity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
