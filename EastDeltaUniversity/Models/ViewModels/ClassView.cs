488a86ef175aa02f572db051ffb3f7c39b06ce35
IrfanulHasan <irfanulhasan1996@gmail.com>
2020-06-22 01:19:24 +0600

    Page Customize and Fix error!

--- EastDeltaUniversity/Models/ViewModels/ClassView.cs
+++ EastDeltaUniversity/Models/ViewModels/ClassView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,16 @@ namespace EastDeltaUniversity.Models.ViewModels
 {
     public class ClassView
     {
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
+        
+        [Display(Name = "Code")]
         public string CourseCode { get; set; }
+        
+        [Display(Name = "Name")]
         public string CourseName { get; set; }
+        
+        [Display(Name = "Course Info")]
         public string CourseInfo { get; set; }
     }
 }
